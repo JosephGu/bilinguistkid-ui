a3a69422383aa7b0ad2b3e110e781217f3cd4dd0
josephgu <285822850@qq.com>
2025-09-21 15:04:30 +0800

    add pwa

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Bilinguist Kid",
+    short_name: "Bilinguist",
+    description: "A website for BilinguistKid",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      {
+        src: "/logo192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/logo512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
