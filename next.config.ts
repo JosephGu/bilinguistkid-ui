afc4a8b3fee3a77b757aed01235a01fafa470df9
josephgu <285822850@qq.com>
2025-11-06 11:18:42 +0800

    avoid sourcemaps

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   async rewrites() {
+  productionBrowserSourceMaps: false,
+  async rewrites() {
     return [
       {
-        source: '/api/:path*', // 前端请求的路径（如 /api/user → 代理到后端）
-        destination: 'http://localhost:8080/api/:path*', // 后端 API 地址
+        source: "/api/:path*", // 前端请求的路径（如 /api/user → 代理到后端）
+        destination: "http://localhost:8080/api/:path*", // 后端 API 地址
       },
       // 可以配置多个代理规则
-     
     ];
   },
 };
