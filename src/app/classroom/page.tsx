3681a5282219bfaf3c7e38a36fe87436da953285
josephgu <285822850@qq.com>
2025-12-23 17:20:22 +0800

    add peerjs for classroom

--- src/app/classroom/page.tsx
+++ src/app/classroom/page.tsx
@@ -2,9 +2,49 @@
 
 import { useEffect } from "react";
 
+import { Peer } from "peerjs";
+
 import { Dialog, Box, Button, TextField } from "@mui/material";
 
 function ClassroomPage() {
+  const peer = new Peer();
+
+  useEffect(() => {}, []);
+
+  const launchClass = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        const call = peer.call("another-peers-id", stream);
+        call.on("stream", (remoteStream) => {
+          // Show stream in some <video> element.
+        });
+      })
+      .catch((err) => {
+        console.error("Failed to get local stream", err);
+      });
+  };
+
+  const joinClass = () => {
+    peer.on("call", (call) => {
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: true })
+        .then((stream) => {
+          call.answer(stream); // Answer the call with an A/V stream.
+          call.on("stream", (remoteStream) => {
+            // Show stream in some <video> element.
+            const video = document.createElement("video");
+            video.srcObject = remoteStream;
+            video.play();
+            document.body.appendChild(video);
+          });
+        })
+        .catch((err) => {
+          console.error("Failed to get local stream", err);
+        });
+    });
+  };
+
   return (
     <Dialog open fullWidth>
       <Box className="m-8 align-items-center flex justify-center flex-col">
@@ -16,8 +56,8 @@ function ClassroomPage() {
           ></TextField>
         </Box>
         <Box className="flex flex-row items-center justify-center">
-          <Button>Launch Class</Button>
-          <Button>Join Class</Button>
+          <Button onClick={launchClass}>Launch Class</Button>
+          <Button onClick={joinClass}>Join Class</Button>
         </Box>
       </Box>
     </Dialog>
